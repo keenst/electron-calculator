e941a0215844abc371b9d660039949ef5de1458c
BuildTools <unconfigured@null.spigotmc.org>
2022-12-06 18:46:48 +0100

    Fixed buffer getting too long

--- src/App.vue
+++ src/App.vue
@@ -22,15 +22,15 @@
       </div>
       <div class="h-[200px] flex flex-col items-end justify-end">
         <div class="
-        text-orange-100 text-2xl pr-4 select-none dark:text-blue-600"> {{ getDisplayNum(memory) }} </div>
+        text-orange-100 text-2xl pr-4 select-none dark:text-blue-600"> {{ memoryDisplay }} </div>
         <div class="
         pr-4 pb-4 h-20 font-semibold text-orange-300 flex flex-col items-end justify-end no-drag
         dark:text-blue-400 ease-out duration-300 cursor-text
         " :class="{
-          [`text-6xl`]: buffer.length <= 12,
-          [`text-5xl`]: buffer.length > 12,
-          [`text-4xl`]: buffer.length > 15,
-        }"> {{ getDisplayNum(buffer) }} </div>
+          [`text-6xl`]: bufferDisplay.length <= 11,
+          [`text-5xl`]: bufferDisplay.length > 11,
+          [`text-4xl`]: bufferDisplay.length > 14
+        }"> {{ bufferDisplay }} </div>
       </div>
       <div class="grid grid-cols-4 gap-1 m-2 grow no-drag">
         <key
@@ -45,126 +45,8 @@
 </template>
 
 <script>
-import key from './components/Key'
-import nerdamer from 'nerdamer'
-import CloseIcon from 'icons/Close'
-import DarkModeIcon from 'icons/WeatherNight'
-import LightModeIcon from 'icons/WhiteBalanceSunny'
-
-const { ipcRenderer } = window.require('electron')
-
-export default {
-  setup() {
-    
-  },
-  components: {
-    key,
-    CloseIcon,
-    DarkModeIcon,
-    LightModeIcon
-  },
-  data() {
-    return {
-      buffer: '0',
-      memory: '',
-      buttons: [
-        'CE', '^', '/', 'DEL',
-        '7', '8', '9', '*',
-        '4', '5', '6', '-',
-        '1', '2', '3', '+',
-        '+/-', '0', '.', '='
-      ],
-      darkMode: true
-    }
-  },
-  methods: {
-    addToBuffer(input) {
-      switch (input) {
-        case 'C':
-          this.buffer = '0'
-          this.memory = ''
-          break;
-        case 'CE':
-          this.buffer = '0'
-          break
-        
-        case 'DEL':
-          if (this.buffer.length == 1)
-            this.buffer = '0'
-          else  
-            this.buffer = this.buffer.substring(0, this.buffer.length - 1)
-          break
-        
-        case '=':
-          this.memory += this.buffer
-          var e = nerdamer(this.memory)
-          this.buffer = e.text('decimals')
-          this.memory = ''
-          break
-
-        case '+/-':
-          if (this.buffer.charAt(0) == '-')
-            this.buffer = this.buffer.substring(1)
-          else
-            this.buffer = '-' + this.buffer;
-          break;
-
-        case '.':
-          if (!this.buffer.includes('.'))
-            this.buffer += '.'
-          break;
-
-        case '+':
-        case '-':
-        case '*':
-        case '^':
-        case '/':
-          this.memory += ' ' + this.buffer
-          this.buffer = '0'
-
-          if (this.memory.split(' ').length > 0) {
-            console.log('longer than 0')
-            var e = nerdamer(this.memory)
-            this.memory = e.text('decimals')
-          }
-
-          this.memory += ' ' + input
-
-          break
-
-        default:
-          if (this.buffer.length >= 20) return
-
-          if (this.buffer == '0')
-            this.buffer = input
-          else
-            this.buffer += input
-      }
-
-      if (this.buffer == '0' && this.memory.length > 0)
-        this.buttons[0] = 'C'
-      else 
-        this.buttons[0] = 'CE'
-    },
-    getDisplayNum(input) {
-      if (input.length > 20) 
-        return Number.parseFloat(input).toExponential(17)
-      else {
-        if (!input.includes('.')) {
-          return input.replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
-        }
-
-        let numbers = input.split('.')[0]
-        let decimals = input.split('.')[1]
-        numbers = numbers.replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
-        return numbers + '.' + decimals
-      }
-    },
-    closeApp() {
-      ipcRenderer.send('close', true)
-    }
-  }
-}
+  import App from './app.js'
+  export default App
 </script>
 
 <style>
