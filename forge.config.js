6ce19924732229edb9dd795e3f9c036c7d822cf4
keenst <leo.siven@icloud.com>
2022-12-08 15:29:36 +0100

    started setting up forge

---
+++ forge.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  publishers: {
+    name: '@electron-forge/publisher-github',
+    config: {
+      repository: {
+        owner: 'keenst',
+        name: 'electron-calculator'
+      },
+      prerelease: false,
+      draft: true
+    }
+  },
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
